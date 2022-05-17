f386066ebb58de7b7bcdfbbb630ac56ec759e178
AkbarAli <88106359+satipoff@users.noreply.github.com>
2022-05-17 17:03:01 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, "templates"),
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
